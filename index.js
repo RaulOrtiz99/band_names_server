a51ad79209ece84b96442f471d48339c087bc3e1
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-07-28 01:33:06 -0400

    agregando directorios para que los lea con el public path

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
 const app = express();
+const path= require('path');
+
+//path publico
+const publicPath= path.resolve(__dirname,'public');
+
+app.use(express.static(publicPath));
 
 app.listen(3000,(err)=>{
    if (err) throw new Error(err);
