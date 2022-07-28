4b3ccbbcb305cd6b5b6f8365e4167d27de68467b
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2022-07-28 01:39:33 -0400

    agregando el archivo .env para el puerto

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require('express');
 
 const app = express();
 const path= require('path');
+require('dotenv').config();
 
-//path publico
+// TODO path publico
 const publicPath= path.resolve(__dirname,'public');
 
 app.use(express.static(publicPath));
 
-app.listen(3000,(err)=>{
+app.listen(process.env.PORT,(err)=>{
    if (err) throw new Error(err);
 
-   console.log('Servidor corriendo en puerto!!!!',3000);
+   console.log('Servidor corriendo en puerto!!!!',process.env.PORT);
 });
