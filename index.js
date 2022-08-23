ab7e25d5535db95b652b63f923a887e2a15a61e7
RaulOrtiz99 <raulalbertoortiz290199@hotmai.com>
2022-08-23 01:15:08 -0400

    Agregando los eventos podemos escuchar y emitir

--- index.js
+++ index.js
@@ -14,6 +14,12 @@ io.on('connection', client => {
    client.on('disconnect',()=>{
       console.log('Client disconnected');
    });
+
+   client.on('mensaje', (payload) => {
+      console.log('Mensaje',payload)
+
+      io.emit('mensaje',{admin:'Nuevo mensaje'});
+   })
 });
 
 
