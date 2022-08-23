603338c62c6c8d7e09f0c8bef37de08ad96fe07d
RaulOrtiz99 <raulalbertoortiz290199@hotmai.com>
2022-08-23 01:01:33 -0400

    agregando el socket desde la parte del cliente

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
-
+//app de express
 const app = express();
 const path= require('path');
 require('dotenv').config();
 
 // node server
-const server = require('http').createServer();
+const server = require('http').createServer(app);
+const io= require('socket.io')(server);
+
+//Mensajes de sockets
+io.on('connection', client => {
+   console.log('Client connected');
+   client.on('disconnect',()=>{
+      console.log('Client disconnected');
+   });
+});
+
 
 // TODO path publico
 const publicPath= path.resolve(__dirname,'public');
 
 app.use(express.static(publicPath));
 
-app.listen(process.env.PORT,(err)=>{
+server.listen(process.env.PORT,(err)=>{
    if (err) throw new Error(err);
 
    console.log('Servidor corriendo en puerto!!!!',process.env.PORT);
